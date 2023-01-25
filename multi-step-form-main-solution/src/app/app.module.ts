c30b9379704f01674dea03070678acc397f68468
Darvas Péter <darvas.peter@bpc.hu>
2023-01-25 14:56:37 +0100

    Multi step form challenge solution initial commit

---
+++ multi-step-form-main-solution/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule, isDevMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MultiFormComponent } from './compontents/multi-form/multi-form.component';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MultiFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}, {}),
+    EffectsModule.forRoot([]),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ multi-step-form-main-solution/src/app/compontents/multi-form/multi-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multi-form',
+  templateUrl: './multi-form.component.html',
+  styleUrls: ['./multi-form.component.scss']
+})
+export class MultiFormComponent {
+
+}
