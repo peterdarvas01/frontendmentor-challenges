e9147037d22f35958a1beafbd70b4fa349257c72
Darvas Péter <darvas.peter@bpc.hu>
2023-01-26 14:13:04 +0100

    Multi step form - sidebar highlight

--- multi-step-form-main-solution/src/app/multi-form/components/header/header.component.ts
+++ multi-step-form-main-solution/src/app/multi-form/components/header/header.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppStateInterface } from 'src/app/types/appState.interface';
+import { currentPageSelector } from '../../store/selectors';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  currentPage$: Observable<number>;
 
+  constructor(private store: Store<AppStateInterface>) {
+    this.currentPage$ = this.store.pipe(select(currentPageSelector));
+  }
 }
